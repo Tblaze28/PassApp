1e1f9ac358ce826f7bc03aef2c593775bc5c853b
tblaze28 <trevorblazek28@gmail.com>
2025-04-29 16:33:27 -0400

    Fix port detection for Render

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def logout():
     return redirect(url_for('login'))
 
 # --- Start Server ---
-iif __name__ == "__main__":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
